0da2c17e95f5b75bffabe393aa91b6dcf9555f0a
A00456016 <90929458+A00456016@users.noreply.github.com>
2022-02-28 01:30:31 -0400

    some more changes

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-
-async function main() {
-    
-    if ('serviceWorker' in navigator) { // checking if the browser supports service workers
-        window.addEventListener('load', function () { // when app loads, fire callback
-            navigator.serviceWorker.register('/sw.js').then(function () { // register sw
-                console.log('ServiceWorker registration successful');  // registration was successful
-            }, function (err) {
-                console.log('ServiceWorker registration failed', err); // registration failed
-            });
+if ('serviceWorker' in navigator) { // checking if the browser supports service workers
+    window.addEventListener('load', function () { // when app loads, fire callback
+        navigator.serviceWorker.register('/sw.js').then(function () { // register sw
+            console.log('ServiceWorker registration successful');  // registration was successful
+        }, function (err) {
+            console.log('ServiceWorker registration failed', err); // registration failed
         });
-    }
+    });
+}
    
-
+async function main() {
 
 const form = document.querySelector('form');
 const name_in = document.querySelector("[name='taskname']");
